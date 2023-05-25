e2f36d69432196e271240cfae620ad7574301c4a
@hexlet/cli <support@hexlet.io>
2023-05-25 23:20:48 +0300

    submit java-spring-ru/filtration

--- java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
+++ java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
@@ -1,20 +1,14 @@
 package exercise.controller;
 
-import exercise.model.Condition;
 import exercise.model.QUser;
 import exercise.model.User;
 import exercise.repository.UserRepository;
-import exercise.service.SearchCriteria;
 //import exercise.service.UserSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 // Зависимости для самостоятельной работы
 // import org.springframework.data.querydsl.binding.QuerydslPredicate;
